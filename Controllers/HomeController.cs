b103819d366807371029c936847cc91ea838b47d
Abhinav Gangula <abgangul@microsoft.com>
2021-03-28 02:17:36 +0530

    Removing comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,37 +40,11 @@ namespace AspDotNetCppThreejs.Controllers
  
             var content = new FormUrlEncodedContent(parameters);
             HttpResponseMessage response = await httpClient.PostAsync(url, content);
-            // Line breaks for legibility only
-            // Content-Type: application/x-www-form-urlencoded
-            /*
-            string queryString = QueryHelpers.AddQueryString("", parameters);
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url + queryString);
-            request.Headers.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
-            request.Content = new StringContent("application/x-www-form-urlencoded");//CONTENT-TYPE header
-            request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/x-www-form-urlencoded");
-            request.Content.Headers.ContentEncoding.Add("UTF8");
-            response = await httpClient.SendAsync(request);
 
-            httpClient.DefaultRequestHeaders.Clear();
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
-            request = new HttpRequestMessage(HttpMethod.Post, "http://login.microsoftonline.com/common/oauth2/v2.0/token");
-            var keyValues = new List<KeyValuePair<string, string>>();
-            keyValues.Add(new KeyValuePair<string, string>("client_id", "f1e1a7e0-4ae5-490d-91e6-45cc6ec5ee32"));
-            keyValues.Add(new KeyValuePair<string, string>("scope", "user.read"));
-            keyValues.Add(new KeyValuePair<string, string>("code", code));
-            keyValues.Add(new KeyValuePair<string, string>("redirect_uri", "http://localhost:44328/home/addtoken/"));
-            keyValues.Add(new KeyValuePair<string, string>("grant_type", "authorization_code"));
-            keyValues.Add(new KeyValuePair<string, string>("client_secret", "jTVrDY4mP~5.D78q7EZXs3h.Luu8x4.p22"));
-
-            request.Content = new FormUrlEncodedContent(keyValues);
-            request.Method = HttpMethod.Post;
-            response = httpClient.SendAsync(request).Result;
-            */
             if (response.IsSuccessStatusCode)
             {
                 var contents = await response.Content.ReadAsStringAsync();
                 return contents;
-                //return JsonConvert.DeserializeObject<object>(contents);
             }
             else
             {
